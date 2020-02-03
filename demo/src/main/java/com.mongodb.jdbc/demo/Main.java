e60afc8395e24bfa81a11f7b0b5ae0b2c2c298d3
Patrick Meredith <pmeredit@gmail.com>
2020-02-03 14:07:38 -0500

    BI-2452: Update demo

--- demo/src/main/java/com.mongodb.jdbc/demo/Main.java
+++ demo/src/main/java/com.mongodb.jdbc/demo/Main.java
@@ -32,15 +32,14 @@ public class Main {
    public static void main(String[] args) {
 
       try{
-         Class.forName(JDBC_DRIVER);
-		 java.util.Properties p = new java.util.Properties();
-		 // These properties will be added to the URI.
-		 // Uncomment if you wish to specify user and password.
-		 // p.setProperty("user", "user");
-		 // p.setProperty("password", "foo");
-		 p.setProperty("database", "test");
+         java.util.Properties p = new java.util.Properties();
+         // These properties will be added to the URI.
+         // Uncomment if you wish to specify user and password.
+         // p.setProperty("user", "user");
+         // p.setProperty("password", "foo");
+         p.setProperty("database", "test");
          System.out.println("Connecting to database test...");
-         Connection conn = DriverManager.getConnection(URL, "fake_user", "fake_password");
+         Connection conn = DriverManager.getConnection(URL, p);
 
          System.out.println("Creating statement...");
          Statement stmt = conn.createStatement();
