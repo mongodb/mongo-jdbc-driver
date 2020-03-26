b099b3aadbbd222a499632b3b873541e50210971
huan-Mongo <huan.li@mongodb.com>
2020-03-25 22:13:16 -0400

    change publishing with jdk8

--- gradle/publish.sh
+++ gradle/publish.sh
@@ -21,8 +21,7 @@ export ORG_GRADLE_PROJECT_signing_secretKeyRingFile=${PROJECT_DIRECTORY}/secring
 export ORG_GRADLE_PROJECT_nexus_url=${NEXUS_URL}
 export ORG_GRADLE_PROJECT_nexus_profile_id=${NEXUS_PROFILE_ID}
 
-echo "Publishing snapshot with jdk9"
-export JAVA_HOME="/opt/java/jdk9"
+echo "Publishing snapshot"
 
 ./gradlew -version
 ./gradlew publishMaven --info
