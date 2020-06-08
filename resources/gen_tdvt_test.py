13a4b45ed5b481f7b139b3e8a09ca1fb54cd3b39
Patrick Meredith <pmeredit@gmail.com>
2020-06-08 17:10:44 -0400

    SQL-53: 15% more passes based on new checking code

--- resources/gen_tdvt_test.py
+++ resources/gen_tdvt_test.py
@@ -7,9 +7,9 @@ package com.mongodb.jdbc.tdvt;
 
 import static org.junit.Assert.*;
 
+import java.lang.Math;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -36,17 +36,101 @@ public class TDVTTest {
         return DriverManager.getConnection(URL, p);
     }
 
-    static HashSet<ArrayList<String>> buildResultSetSet(ResultSet rs) throws SQLException {
-        HashSet<ArrayList<String>> ret = new HashSet<>();
+    static Object getTestObject(int i, ResultSetMetaData rsmd, ResultSet rs) throws SQLException {
+        switch (rsmd.getColumnType(i)) {
+            case Types.BIT:
+                return rs.getBoolean(i);
+            case Types.TIMESTAMP:
+                return rs.getDate(i);
+            case Types.NULL:
+                return null;
+            case Types.DOUBLE:
+            case Types.DECIMAL:
+                return rs.getDouble(i);
+            case Types.INTEGER:
+                return rs.getLong(i);
+            default:
+                return rs.getString(i);
+        }
+    }
+
+    static boolean compareResults(
+            ArrayList<ArrayList<Object>> expected, ArrayList<ArrayList<Object>> result) {
+        if (expected.size() != result.size()) {
+            System.out.println(
+                    "Expected and result differ in length, "
+                            + expected.size()
+                            + " and "
+                            + result.size()
+                            + ", respectively");
+            return false;
+        }
+        for (int rowIdx = 0; rowIdx < expected.size(); ++rowIdx) {
+            ArrayList<Object> expectedRow = expected.get(rowIdx);
+            ArrayList<Object> resultRow = expected.get(rowIdx);
+            if (expectedRow.size() != resultRow.size()) {
+                System.out.println(
+                        "Expected row and result row differ in length, "
+                                + expectedRow.size()
+                                + " and "
+                                + resultRow.size()
+                                + ", respectively");
+                return false;
+            }
+            for (int colIdx = 0; colIdx < expectedRow.size(); ++colIdx) {
+                Object expectedVal = expectedRow.get(colIdx);
+                Object resultVal = resultRow.get(colIdx);
+                if (expectedVal == null) {
+                    if (resultVal != null) {
+                        System.out.println(
+                                "At position: "
+                                        + rowIdx
+                                        + ", "
+                                        + colIdx
+                                        + " expected value: "
+                                        + expectedVal
+                                        + " != "
+                                        + resultVal);
+                        return false;
+                    }
+                } else if (resultVal instanceof Double) {
+                    if (Math.abs((double) resultVal - (double) expectedVal) > 0.05) {
+                        System.out.println(
+                                "At position: "
+                                        + rowIdx
+                                        + ", "
+                                        + colIdx
+                                        + " expected value: "
+                                        + expectedVal
+                                        + " != "
+                                        + resultVal);
+                        return false;
+                    }
+                } else if (!resultVal.equals(expectedVal)) {
+                    System.out.println(
+                            "At position: "
+                                    + rowIdx
+                                    + ", "
+                                    + colIdx
+                                    + " expected value: "
+                                    + expectedVal
+                                    + " != "
+                                    + resultVal);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static ArrayList<ArrayList<Object>> buildResultSetSet(ResultSet rs) throws SQLException {
+        ArrayList<ArrayList<Object>> ret = new ArrayList<>();
         ResultSetMetaData rsmd = rs.getMetaData();
         int count = rsmd.getColumnCount();
         while(rs.next()) {
-            ArrayList<String> row = new ArrayList<>(count);
-            for(int i = 0; i < count; ++i) {
-                String result = rs.getString(i+1);
-                if( result != null) {
-                     result = result.replaceAll("true", "1").replaceAll("false", "0");
-                }
+            ArrayList<Object> row = new ArrayList<>(count);
+            for(int i = 1; i <= count; ++i) {
+                Object result = getTestObject(i, rsmd, rs);
                 row.add(result);
             }
             ret.add(row);
@@ -88,16 +172,22 @@ def clean_sql(sql):
         sql = sql.replace(key, key[0:-1])
     return sql
 
-def make_col_str(col):
+def make_col_str(ty, col):
+    ty = types[ty]
     if col == '~':
         return 'null'
     if col is None:
         return 'null'
     if col == 'NULL':
         return 'null'
-    if type(col) == str:
-        return '"' + col + '"'
-    return '"' + str(col) + '"'
+    if ty == 'string' or ty == 'date':
+        return '"' + str(col) + '"'
+    if ty == 'bool':
+        if col == '0':
+            return 'false'
+        return 'true'
+    if ty == 'long' or ty == 'double' or ty == 'decimal':
+        return str(col)
 
 def make_test(fName, test):
     # we are only doing tests expected to run correctly.
@@ -132,22 +222,22 @@ def make_test(fName, test):
             print('        assertTrue("failed type check", tyName.equals("' + types[ty] + '"));')
     if 'expected_results' in test and len(test['expected_results']) > 0:
         # Just compare everything as strings.
-        print('        HashSet<ArrayList<String>> expected = new HashSet<>(' + str(len(test['expected_results'])) + ');')
-        print('        ArrayList<String> expectedRow;')
+        print('        ArrayList<ArrayList<Object>> expected = new ArrayList<>(' + str(len(test['expected_results'])) + ');')
+        print('        ArrayList<Object> expectedRow;')
         for row in test['expected_results']:
             print('        expectedRow = new ArrayList<>(' + str(len(row)) + ');')
-            for col in row:
-                col_str = make_col_str(col)
+            for i, col in enumerate(row):
+                col_str = make_col_str(test['expected_types'][i], col)
                 print('        expectedRow.add(' + col_str + ');')
             print('        expected.add(expectedRow);')
-        print('        HashSet<ArrayList<String>> rsSet = buildResultSetSet(rs);')
+        print('        ArrayList<ArrayList<Object>> rsSet = buildResultSetSet(rs);')
         # We'll leave the printouts here until we are done fixing the failing tests in a future
         # ticket.
         print('        System.out.println(expected.toString());')
         print('        System.out.println(rsSet.toString());')
-        print('        if (rsSet.retainAll(expected)) {')
+        print('        if (!compareResults(expected, rsSet)) {')
         print('        System.out.println("Failed: " + rsSet.toString());')
-        print('        // This will be false if both HashSets are the same')
+        print('        // This will be false if both ArrayLists are the same')
         print('        assertTrue("failed result check", false);')
         print('        }')
     print("    }\n")
