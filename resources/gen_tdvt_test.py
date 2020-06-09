8294f6f5f08ae1cfe6c1da2433396259ef302384
Patrick Meredith <pmeredit@gmail.com>
2020-06-09 11:40:08 -0400

    SQL-53: Improvements to test infrustructure and fixes to tests (#31)

--- resources/gen_tdvt_test.py
+++ resources/gen_tdvt_test.py
@@ -7,16 +7,24 @@ package com.mongodb.jdbc.tdvt;
 
 import static org.junit.Assert.*;
 
+import java.lang.Math;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(TDVTTest.class)
 public class TDVTTest {
     static final String URL = "jdbc:mongodb://" + System.getenv("ADL_TEST_HOST") + "/tdvt";
+    static Connection conn;
 
+    static {
+        try {
+            conn =  getBasicConnection();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     static Connection getBasicConnection() throws SQLException {
         java.util.Properties p = new java.util.Properties();
@@ -28,14 +36,105 @@ public class TDVTTest {
         return DriverManager.getConnection(URL, p);
     }
 
-    static HashSet<ArrayList<String>> buildResultSetSet(ResultSet rs) throws SQLException {
-        HashSet<ArrayList<String>> ret = new HashSet<>();
+    static Object getTestObject(int i, ResultSetMetaData rsmd, ResultSet rs) throws SQLException {
+        Object ret;
+        switch (rsmd.getColumnType(i)) {
+            case Types.BIT:
+                ret = rs.getBoolean(i);
+                return rs.wasNull()?null:ret;
+            case Types.NULL:
+                return null;
+            case Types.DOUBLE:
+            case Types.DECIMAL:
+                ret = rs.getDouble(i);
+                return rs.wasNull()?null:ret;
+            case Types.INTEGER:
+                ret = rs.getLong(i);
+                return rs.wasNull()?null:ret;
+            default:
+                ret = rs.getString(i);
+                return rs.wasNull()?null:ret;
+        }
+    }
+
+    static boolean compareResults(
+            ArrayList<ArrayList<Object>> expected, ArrayList<ArrayList<Object>> result) {
+        if (expected.size() != result.size()) {
+            System.out.println(
+                    "Expected and result differ in length, "
+                            + expected.size()
+                            + " and "
+                            + result.size()
+                            + ", respectively");
+            return false;
+        }
+        for (int rowIdx = 0; rowIdx < expected.size(); ++rowIdx) {
+            ArrayList<Object> expectedRow = expected.get(rowIdx);
+            ArrayList<Object> resultRow = result.get(rowIdx);
+            if (expectedRow.size() != resultRow.size()) {
+                System.out.println(
+                        "Expected row and result row differ in length, "
+                                + expectedRow.size()
+                                + " and "
+                                + resultRow.size()
+                                + ", respectively");
+                return false;
+            }
+            for (int colIdx = 0; colIdx < expectedRow.size(); ++colIdx) {
+                Object expectedVal = expectedRow.get(colIdx);
+                Object resultVal = resultRow.get(colIdx);
+                if (expectedVal == null) {
+                    if (resultVal != null) {
+                        System.out.println(
+                                "At position: "
+                                        + rowIdx
+                                        + ", "
+                                        + colIdx
+                                        + " expected value: "
+                                        + expectedVal
+                                        + " != "
+                                        + resultVal);
+                        return false;
+                    }
+                } else if (resultVal instanceof Double) {
+                    if (Math.abs((double) resultVal - (double) expectedVal) > 0.005) {
+                        System.out.println(
+                                "At position: "
+                                        + rowIdx
+                                        + ", "
+                                        + colIdx
+                                        + " expected value: "
+                                        + expectedVal
+                                        + " != "
+                                        + resultVal);
+                        return false;
+                    }
+                } else if (!expectedVal.equals(resultVal)) {
+                    System.out.println(
+                            "At position: "
+                                    + rowIdx
+                                    + ", "
+                                    + colIdx
+                                    + " expected value: "
+                                    + expectedVal
+                                    + " != "
+                                    + resultVal);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static ArrayList<ArrayList<Object>> buildResultSetSet(ResultSet rs) throws SQLException {
+        ArrayList<ArrayList<Object>> ret = new ArrayList<>();
         ResultSetMetaData rsmd = rs.getMetaData();
         int count = rsmd.getColumnCount();
         while(rs.next()) {
-            ArrayList<String> row = new ArrayList<>(count);
-            for(int i = 0; i < count; ++i) {
-                row.add(rs.getString(i+1));
+            ArrayList<Object> row = new ArrayList<>(count);
+            for(int i = 1; i <= count; ++i) {
+                Object result = getTestObject(i, rsmd, rs);
+                row.add(result);
             }
             ret.add(row);
         }
@@ -47,9 +146,13 @@ public class TDVTTest {
 print(test_text)
 
 types = {
+        None: 'null',
+        'null': 'null',
         'varchar': 'string',
         'string': 'string',
-        'int': 'int',
+        'bool': 'bool',
+        'int': 'long',
+        'long': 'long',
         'float': 'double',
         'double': 'double',
         'date': 'date',
@@ -66,7 +169,7 @@ def clean_sql(sql):
     # Our column names in Staples are all Capitalized, so replace back to the capitalized form. The
     # queries were originally written for case insensitive MySQL.
     for key in [
- "Item Count" , "Ship Priority" , "Order Priority" , "Order Status" , "Order Quantity" , "Sales Total" , "Discount" , "Tax Rate" , "Ship Mode" , "Fill Time" , "Gross Profit" , "Price" , "Ship Handle Cost" , "Employee Name" , "Employee Dept" , "Manager Name" , "Employee Yrs Exp" , "Employee Salary" , "Customer Name" , "Customer State" , "Call Center Region" , "Customer Balance" , "Customer Segment" , "Prod Type1" , "Prod Type2" , "Prod Type3" , "Prod Type4" , "Product Name" , "Product Container" , "Ship Promo" , "Supplier Name" , "Supplier Balance" , "Supplier Region" , "Supplier State" , "Order ID" , "Order Year" , "Order Month" , "Order Day" , "Order Date" , "Order Quarter" , "Product Base Margin" , "Product ID" , "Receive Time" , "Received Date" , "Ship Date" , "Ship Charge" , "Total Cycle Time" , "Product In Stock" , "PID" , "Market Segment "	]:
+ "Item Count" , "Ship Priority" , "Order Priority" , "Order Status" , "Order Quantity" , "Sales Total" , "Discount" , "Tax Rate" , "Ship Mode" , "Fill Time" , "Gross Profit" , "Price" , "Ship Handle Cost" , "Employee Name" , "Employee Dept" , "Manager Name" , "Employee Yrs Exp" , "Employee Salary" , "Customer Name" , "Customer State" , "Call Center Region" , "Customer Balance" , "Customer Segment" , "Prod Type1" , "Prod Type2" , "Prod Type3" , "Prod Type4" , "Product Name" , "Product Container" , "Ship Promo" , "Supplier Name" , "Supplier Balance" , "Supplier Region" , "Supplier State" , "Order ID" , "Order Year" , "Order Month" , "Order Day" , "Order Date" , "Order Quarter" , "Product Base Margin" , "Product ID" , "Receive Time" , "Received Date" , "Ship Date" , "Ship Charge" , "Total Cycle Time" , "Product In Stock" , "PID" , "Market Segment "    ]:
         sql = sql.replace(key.lower(), key)
     # tdvt has a mode where bool columns end with _. We did not map the columns this way in ADL, so
     # we need to remove those underscores. E.g., we replace bool0_ with bool0.
@@ -74,16 +177,26 @@ def clean_sql(sql):
         sql = sql.replace(key, key[0:-1])
     return sql
 
-def make_col_str(col):
+def make_col_str(ty, col):
+    ty = types[ty]
     if col == '~':
         return 'null'
     if col is None:
         return 'null'
     if col == 'NULL':
         return 'null'
-    if type(col) == str:
-        return '"' + col + '"'
-    return '"' + str(col) + '"'
+    if ty == 'null':
+        return 'null'
+    if ty == 'string' or ty == 'date':
+        return '"' + str(col) + '"'
+    if ty == 'bool':
+        if col == '0':
+            return 'false'
+        return 'true'
+    if ty == 'long':
+        return str(long(col)) + 'l'
+    if ty == 'double' or ty == 'decimal':
+        return str(float(col)) + 'd'
 
 def make_test(fName, test):
     # we are only doing tests expected to run correctly.
@@ -97,7 +210,7 @@ def make_test(fName, test):
     testName = fName + test["id"]
     print("    @Test")
     print("    public void test" + testName + "() throws SQLException {")
-    print("        Connection conn = getBasicConnection();")
+    print('        System.out.println("=============================='+ testName +'");')
     print("        Statement stmt = conn.createStatement();")
     sql = clean_sql(test["sql"])
     print('        ResultSet rs = stmt.executeQuery("' + sql + '");')
@@ -110,27 +223,31 @@ def make_test(fName, test):
         print('        String tyName;')
         for i, ty in enumerate(test['expected_types']):
             print('        tyName = rsmd.getColumnTypeName(' + str(i+1) + ');')
+            print('if(!tyName.equals("' + types[ty] + '")) {')
+            print('    System.out.println(tyName + " == " + "' + types[ty] + '");')
+            print('}')
             # For now, allow the tyName to be null. We will fix this soon when the results from ADL
             # contain the type.
-            print('        assertTrue("failed type check", tyName.equals("null") || tyName.equals("' + types[ty] + '"));')
+            print('        assertTrue("failed type check", tyName.equals("' + types[ty] + '"));')
     if 'expected_results' in test and len(test['expected_results']) > 0:
         # Just compare everything as strings.
-        print('        HashSet<ArrayList<String>> expected = new HashSet<>(' + str(len(test['expected_results'])) + ');')
-        print('        ArrayList<String> expectedRow;')
+        print('        ArrayList<ArrayList<Object>> expected = new ArrayList<>(' + str(len(test['expected_results'])) + ');')
+        print('        ArrayList<Object> expectedRow;')
         for row in test['expected_results']:
             print('        expectedRow = new ArrayList<>(' + str(len(row)) + ');')
-            for col in row:
-                col_str = make_col_str(col)
+            for i, col in enumerate(row):
+                col_str = make_col_str(test['expected_types'][i], col)
                 print('        expectedRow.add(' + col_str + ');')
             print('        expected.add(expectedRow);')
-        print('        HashSet<ArrayList<String>> rsSet = buildResultSetSet(rs);')
+        print('        ArrayList<ArrayList<Object>> rsSet = buildResultSetSet(rs);')
         # We'll leave the printouts here until we are done fixing the failing tests in a future
         # ticket.
-        print('        System.out.println("=============================='+ testName +'");')
-        print('        System.out.println(expected.toString());')
-        print('        System.out.println(rsSet.toString());')
-        print('        // This will be false if both HashSets are the same')
-        print('        assertFalse("failed result check", rsSet.retainAll(expected));')
+        print('        if (!compareResults(expected, rsSet)) {')
+        print('            // This will be false if both ArrayLists are the same')
+        print('            System.out.println(expected.toString());')
+        print('            System.out.println(rsSet.toString());')
+        print('            assertTrue("failed result check", false);')
+        print('        }')
     print("    }\n")
 
 def add_cases(fName):
@@ -138,7 +255,6 @@ def add_cases(fName):
 
     for test in y['testcases']:
         make_test(os.path.basename(fName).split('.')[0].upper(), test)
-
 
 for f  in map(lambda x: os.path.join("tdvt_test", x), ['calcs.yml', 'logical_calcs.yml', 'logical_staples.yml']):
     add_cases(f)
