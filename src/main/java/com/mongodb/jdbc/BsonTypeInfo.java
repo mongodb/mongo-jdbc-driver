1cc3cd4a03ebe9298ddcc11a6bc1c62c1908f3ac
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2021-12-17 13:54:07 -0500

    SQL-602: Add BsonType to enum and fix typos

--- src/main/java/com/mongodb/jdbc/BsonTypeInfo.java
+++ src/main/java/com/mongodb/jdbc/BsonTypeInfo.java
@@ -1,32 +1,34 @@
 package com.mongodb.jdbc;
 
+import org.bson.BsonType;
 import java.sql.Types;
 
 public enum BsonTypeInfo {
-    BSON_DOUBLE("double", Types.DOUBLE, false, 15, 15, 2, 15, 15, 8),
-    BSON_STRING("string", Types.LONGNVARCHAR, true, 0, 0, 0, null, null, null),
-    BSON_OBJECT("object", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_ARRAY("array", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_BINDATA("bindData", Types.BINARY, false, 0, 0, 0, null, null, null),
-    BSON_UNDEFINED("undefined", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_OBJECTID("objectId", Types.OTHER, false, 0, 0, 0, 24, null, null),
-    BSON_BOOLEAN("boolean", Types.BIT, false, 0, 0, 0, 1, null, 1),
-    BSON_DATE("date", Types.TIMESTAMP, false, 0, 3, 0, 24, 3, 8),
-    BSON_NULL("null", Types.NULL, false, 0, 0, 0, null, null, null),
-    BSON_REGEX("regex", Types.OTHER, true, 0, 0, 0, null, null, null),
-    BSON_DBPOINTER("dbPointer", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_JAVASCRIPT("javascript", Types.OTHER, true, 0, 0, 0, null, null, null),
-    BSON_SYMBOL("symbol", Types.OTHER, true, 0, 0, 0, null, null, null),
-    BSON_JAVASCRIPTWITHSCOPE("javascriptWithScope", Types.OTHER, true, 0, 0, 0, null, null, null),
-    BSON_INT("int", Types.INTEGER, false, 0, 0, 2, 10, null, 4),
-    BSON_TIMESTAMP("timestamp", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_LONG("long", Types.BIGINT, false, 0, 0, 2, 19, null, 8),
-    BSON_DECIMAL("decimal", Types.DECIMAL, false, 34, 34, 10, 34, 34, 16),
-    BSON_MINKEY("minKey", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_MAXKEY("maxKey", Types.OTHER, false, 0, 0, 0, null, null, null),
-    BSON_BSON("maxKey", Types.OTHER, false, 0, 0, 0, null, null, null);
+    BSON_DOUBLE("double", BsonType.DOUBLE, Types.DOUBLE, false, 15, 15, 2, 15, 15, 8),
+    BSON_STRING("string", BsonType.STRING, Types.LONGNVARCHAR, true, 0, 0, 0, null, null, null),
+    BSON_OBJECT("object", BsonType.DOCUMENT, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_ARRAY("array", BsonType.ARRAY, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_BINDATA("binData", BsonType.BINARY, Types.BINARY, false, 0, 0, 0, null, null, null),
+    BSON_UNDEFINED("undefined", BsonType.UNDEFINED, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_OBJECTID("objectId", BsonType.OBJECT_ID, Types.OTHER, false, 0, 0, 0, 24, null, null),
+    BSON_BOOLEAN("boolean", BsonType.BOOLEAN, Types.BIT, false, 0, 0, 0, 1, null, 1),
+    BSON_DATE("date", BsonType.DATE_TIME, Types.TIMESTAMP, false, 0, 3, 0, 24, 3, 8),
+    BSON_NULL("null", BsonType.NULL, Types.NULL, false, 0, 0, 0, null, null, null),
+    BSON_REGEX("regex", BsonType.REGULAR_EXPRESSION, Types.OTHER, true, 0, 0, 0, null, null, null),
+    BSON_DBPOINTER("dbPointer", BsonType.DB_POINTER, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_JAVASCRIPT("javascript", BsonType.JAVASCRIPT, Types.OTHER, true, 0, 0, 0, null, null, null),
+    BSON_SYMBOL("symbol", BsonType.SYMBOL, Types.OTHER, true, 0, 0, 0, null, null, null),
+    BSON_JAVASCRIPTWITHSCOPE("javascriptWithScope", BsonType.JAVASCRIPT_WITH_SCOPE, Types.OTHER, true, 0, 0, 0, null, null, null),
+    BSON_INT("int", BsonType.INT32, Types.INTEGER, false, 0, 0, 2, 10, null, 4),
+    BSON_TIMESTAMP("timestamp", BsonType.TIMESTAMP, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_LONG("long", BsonType.INT64, Types.BIGINT, false, 0, 0, 2, 19, null, 8),
+    BSON_DECIMAL("decimal", BsonType.DECIMAL128, Types.DECIMAL, false, 34, 34, 10, 34, 34, 16),
+    BSON_MINKEY("minKey", BsonType.MIN_KEY, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_MAXKEY("maxKey", BsonType.MAX_KEY, Types.OTHER, false, 0, 0, 0, null, null, null),
+    BSON_BSON("bson", null, Types.OTHER, false, 0, 0, 0, null, null, null);
 
     private final String bsonName;
+    private final BsonType bsonType;
     private final int jdbcType;
     private final boolean caseSensitivity;
     private final int minScale;
@@ -36,8 +38,9 @@ public enum BsonTypeInfo {
     private final Integer decimalDigits;
     private final Integer charOctetLength;
 
-    BsonTypeInfo(String bsonName, int jdbcType, boolean caseSensitivity, int minScale, int maxScale, int numPrecRadix, Integer precision, Integer decimalDigits, Integer charOctetLength) {
+    BsonTypeInfo(String bsonName, BsonType bsonType, int jdbcType, boolean caseSensitivity, int minScale, int maxScale, int numPrecRadix, Integer precision, Integer decimalDigits, Integer charOctetLength) {
         this.bsonName = bsonName;
+        this.bsonType = bsonType;
         this.jdbcType = jdbcType;
         this.caseSensitivity = caseSensitivity;
         this.minScale = minScale;
@@ -50,6 +53,10 @@ public enum BsonTypeInfo {
 
     public String getBsonName() {
         return bsonName;
+    }
+
+    public BsonType getBsonType() {
+        return bsonType;
     }
 
     public int getJdbcType() {
