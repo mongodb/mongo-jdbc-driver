e21d733a8b4d51e38403fde27d4959f50afc0ff4
Natacha Bagnard <>
2022-01-26 12:02:02 -0800

    Return 'TABLE' as table type for Collection

--- src/main/java/com/mongodb/jdbc/MongoListCollectionsResult.java
+++ src/main/java/com/mongodb/jdbc/MongoListCollectionsResult.java
@@ -1,6 +1,15 @@
 package com.mongodb.jdbc;
 
 public class MongoListCollectionsResult {
+    public static final String TABLE = "TABLE";
+    public static final String COLLECTION = "COLLECTION";
+
     public String name;
     public String type;
+
+    public void setType(String type) {
+        // If mongodb type is collection, map it as TABLE.
+        // Otherwise, keep the type as is.
+        this.type = type.equalsIgnoreCase(COLLECTION) ? TABLE : type;
+    }
 }
