257a1a8091e55307a85fa587f97794b9c40925f3
Natacha Bagnard <>
2022-02-04 16:44:09 -0800

    Forgot 1 import

--- src/main/java/com/mongodb/jdbc/MongoJsonSchema.java
+++ src/main/java/com/mongodb/jdbc/MongoJsonSchema.java
@@ -6,6 +6,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
