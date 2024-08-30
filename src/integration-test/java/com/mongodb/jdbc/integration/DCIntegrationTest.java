f618d5bd989fcb2ba54252e71b564d172c79626d
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2024-08-30 14:09:59 -0400

    SQL-2293: Add integration tests for connecting to community and enterprise servers

--- src/integration-test/java/com/mongodb/jdbc/integration/DCIntegrationTest.java
+++ src/integration-test/java/com/mongodb/jdbc/integration/DCIntegrationTest.java
@@ -2,8 +2,10 @@ package com.mongodb.jdbc.integration;
 
 
 import com.mongodb.jdbc.MongoConnection;
+import com.mongodb.jdbc.Pair;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.function.Executable;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,7 +18,7 @@ public class DCIntegrationTest {
 
     /** Tests that the driver can work with SRV-style URIs. */
     @Test
-    public void testConnectWithSRVURI() throws SQLException {
+    public void testConnectWithSRVURI() {
         String mongoHost = System.getenv("SRV_TEST_HOST");
         assertNotNull(mongoHost, "SRV_TEST_HOST variable not set in environment");
         String mongoURI =
@@ -47,7 +49,50 @@ public class DCIntegrationTest {
                 });
     }
 
-    // todo:
-    //   - test community cluster expects SQLException
-    //   - test enterprise cluster expects successful connection
+    /**
+     * Gets information from the environment to create a connection to a local mongod.
+     *
+     * @param typeEnvVar Either "MDB_LOCAL_PORT_COM" or "MDB_LOCAL_PORT_ENT"
+     * @return A (jdbc_uri, properties) pair with which to create a MongoConnection
+     */
+    private Pair<String, Properties> createLocalMongodConnInfo(String typeEnvVar) {
+        String mongoPort = System.getenv(typeEnvVar);
+        assertNotNull(mongoPort, typeEnvVar + " variable not set in environment");
+
+        String uri = "jdbc:mongodb://localhost:" + mongoPort + "/test";
+
+        String user = System.getenv("MDB_LOCAL_USER");
+        assertNotNull(user, "MDB_LOCAL_USER variable not set in environment");
+        String pwd = System.getenv("MDB_LOCAL_PWD");
+        assertNotNull(pwd, "MDB_LOCAL_PWD variable not set in environment");
+
+        Properties p = new java.util.Properties();
+        p.setProperty("user", user);
+        p.setProperty("password", pwd);
+        p.setProperty("authSource", "admin");
+        p.setProperty("database", "test");
+
+        return new Pair<>(uri, p);
+    }
+
+    /** Tests that the driver rejects the community edition of the server. */
+    @Test
+    public void testConnectionToCommunityServerFails() {
+        Pair<String, Properties> info = createLocalMongodConnInfo("LOCAL_MDB_PORT_COM");
+
+        assertThrows(
+                java.sql.SQLException.class,
+                () -> {
+                    MongoConnection conn =
+                            (MongoConnection) DriverManager.getConnection(info.left(), info.right());
+                });
+    }
+
+    /** Tests that the driver connects to the enterprise edition of the server. */
+    @Test
+    public void testConnectionToEnterpriseServerSucceeds() throws SQLException {
+        Pair<String, Properties> info = createLocalMongodConnInfo("LOCAL_MDB_PORT_ENT");
+        MongoConnection conn = (MongoConnection) DriverManager.getConnection(info.left(), info.right());
+        conn.close();
+    }
 }
