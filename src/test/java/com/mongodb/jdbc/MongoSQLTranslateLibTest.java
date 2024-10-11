6046ac3e90dde577e300dec9b63815e1acec115d
Natacha Bagnard <natacha.bagnard@mongodb.com>
2024-10-11 15:21:26 -0700

    Spotless

--- src/test/java/com/mongodb/jdbc/MongoSQLTranslateLibTest.java
+++ src/test/java/com/mongodb/jdbc/MongoSQLTranslateLibTest.java
@@ -28,23 +28,27 @@ public class MongoSQLTranslateLibTest {
     /**
      * Helper function to call the runCommand endpoint of the translation library.
      */
+
+    /** Helper function to call the runCommand endpoint of the translation library. */
     private static void testRunCommand() {
         MongoSQLTranslate mongosqlTranslate = new MongoSQLTranslate(null);
-        byte[] bytes = mongosqlTranslate.runCommand("SendingSomething".getBytes(StandardCharsets.UTF_8));
+        byte[] bytes =
+                mongosqlTranslate.runCommand("SendingSomething".getBytes(StandardCharsets.UTF_8));
         assert bytes.length > 0;
     }
-    
+
     @BeforeEach
     void setup() throws Exception {
         // Reset the mongoSqlTranslateLibraryLoaded flag to false before each test case.
         // This ensures that the flag starts with a known value at the start of the test
         // as it can be set during the static initialization or test interference.
-        Field mongoSqlTranslateLibraryLoadedField = MongoDriver.class.getDeclaredField("mongoSqlTranslateLibraryLoaded");
+        Field mongoSqlTranslateLibraryLoadedField =
+                MongoDriver.class.getDeclaredField("mongoSqlTranslateLibraryLoaded");
         mongoSqlTranslateLibraryLoadedField.setAccessible(true);
         mongoSqlTranslateLibraryLoadedField.set(null, false);
 
-        Field mongoSqlTranslateLibraryPathField
-                = MongoDriver.class.getDeclaredField("mongoSqlTranslateLibraryPath");
+        Field mongoSqlTranslateLibraryPathField =
+                MongoDriver.class.getDeclaredField("mongoSqlTranslateLibraryPath");
         mongoSqlTranslateLibraryPathField.setAccessible(true);
         mongoSqlTranslateLibraryPathField.set(null, null);
     }
@@ -64,7 +68,10 @@ public class MongoSQLTranslateLibTest {
                 MongoDriver.isMongoSqlTranslateLibraryLoaded(),
                 "Library should be loaded successfully from the driver directory");
 
-        assertTrue (MongoDriver.getMongoSqlTranslateLibraryPath().contains("resources/main"), "Expected library path to contain 'resources/main' but didn't. Actual path is " + MongoDriver.getMongoSqlTranslateLibraryPath());
+        assertTrue(
+                MongoDriver.getMongoSqlTranslateLibraryPath().contains("resources/main"),
+                "Expected library path to contain 'resources/main' but didn't. Actual path is "
+                        + MongoDriver.getMongoSqlTranslateLibraryPath());
 
         // The library was loaded successfully. Now, let's make sure that we can call the runCommand endpoint.
         testRunCommand();
@@ -86,7 +93,11 @@ public class MongoSQLTranslateLibTest {
                 MongoDriver.isMongoSqlTranslateLibraryLoaded(),
                 "Library should be loaded when MONGOSQL_TRANSLATE_PATH is set");
 
-        assertTrue (MongoDriver.getMongoSqlTranslateLibraryPath().contains("resources/MongoSqlLibraryTest"), "Expected library path to contain 'resources/main' but didn't. Actual path is " + MongoDriver.getMongoSqlTranslateLibraryPath());
+        assertTrue(
+                MongoDriver.getMongoSqlTranslateLibraryPath()
+                        .contains("resources/MongoSqlLibraryTest"),
+                "Expected library path to contain 'resources/main' but didn't. Actual path is "
+                        + MongoDriver.getMongoSqlTranslateLibraryPath());
 
         // The library was loaded successfully. Now, let's make sure that we can call the runCommand endpoint.
         testRunCommand();
